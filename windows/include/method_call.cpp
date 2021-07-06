9b46122ceca97cd136aca404547513ab01cf00ad
fahadck50 <86352117+fahadck50@users.noreply.github.com>
2021-07-06 09:57:28 +0530

    Update method_call.cpp
    hWnd in SetFocus cleared

--- windows/include/method_call.cpp
+++ windows/include/method_call.cpp
@@ -108,7 +108,7 @@ namespace DesktopWindowMethodCall
     void MethodCall::focus()
     {
         HWND hWnd = GetActiveWindow();
-        SetFocus(hwnd);
+        SetFocus(hWnd);
 
         result->Success(flutter::EncodableValue(true));
     }
